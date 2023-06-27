1864fe21a09c4c04d25fbe95ac1a8587e3079fc1
melvinswift <greatwinblowaway@gmail.com>
2023-06-28 01:34:16 +0700

    example fe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [oraclePrice, setOraclePrice] = useState<number>();
+
+  useEffect(() => {
+    const getOraclePrice = async () => {
+      const data = await axios
+        .get(
+          "http://localhost:8000/oracle-price/0xff970a61a04b1ca14834a43f5de4533ebddb5cc8"
+        )
+        .then((response) => {
+          return response.data;
+        });
+
+      setOraclePrice(data.priceUSD);
+    };
+
+    getOraclePrice();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-4 flex justify-center items-center h-screen bg-red-200">
+      <p className="text-center">
+        Oracle price of USDC.e is
+        <br />
+        {oraclePrice ? `$${oraclePrice}` : "Loading..."}
+      </p>
     </div>
   );
-}
+};
 
 export default App;
